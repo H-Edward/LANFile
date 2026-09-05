114dcac3e539a589f9be8c5fd6bd00c8c71963fe
Edward Hawkes <edward.hawkes@protonmail.com>
2026-09-05 03:17:33 +0200

    add further preface

--- cmd/lanfile/main.go
+++ cmd/lanfile/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"os"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/H-Edward/LANFile/internal/database"
+	"github.com/H-Edward/LANFile/internal/files"
+	"github.com/H-Edward/LANFile/internal/httpapi"
 	"github.com/joho/godotenv"
 )
 
@@ -15,8 +19,21 @@ func getenv(key, fallback string) string {
 }
 func main() {
 	godotenv.Load()
-	dataDir := getenv("LANFILE_DATA", "/data")
+	dataDir := getenv("LANFILE_DATA", "./data")
 	addr := getenv("LANFILE_ADDR", ":8021")
 
-	http.ListenAndServe(addr, nil)
+	db, err := database.Open(getenv("LANFILE_DB", "./data.db"))
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	if err := database.Ping(db); err != nil {
+		log.Fatalf("failed to ping database: %v", err)
+	}
+
+	fileService := files.NewService(db)
+	router := httpapi.NewRouter(fileService, dataDir)
+
+	log.Printf("Starting server on %s...", addr)
+
+	http.ListenAndServe(addr, router)
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/H-Edward/LANFile
 go 1.27.0
 
 require (
+	github.com/go-chi/chi/v5 v5.3.2
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.50 // indirect
 	golang.org/x/text v0.41.0 // indirect
-	gorm.io/driver/sqlite v1.6.0 // indirect
-	gorm.io/gorm v1.31.2 // indirect
+	gorm.io/driver/sqlite v1.6.0
+	gorm.io/gorm v1.31.2
 )
