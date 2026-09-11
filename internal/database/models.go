eb6d2721e800729bc7c99ac1a1e23b9c0dcd54ee
Edward Hawkes <edward.hawkes@protonmail.com>
2026-09-12 00:55:03 +0200

    Revert storage_key being hidden

--- internal/database/models.go
+++ internal/database/models.go
@@ -4,7 +4,7 @@ import "time"
 
 type File struct {
 	ID           string `gorm:"primaryKey"`
-	StorageKey   string `gorm:"uniqueIndex;not null" json:"-"`
+	StorageKey   string `gorm:"uniqueIndex;not null"`
 	OriginalName string `gorm:"not null"`
 	ContentType  string
 	Size         int64 `gorm:"not null"`
